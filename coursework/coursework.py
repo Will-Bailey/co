b52cb1d49e8d8d499c2b9d94edce76f03206789b
Will-Bailey <will.e.bailey@gmail.com>
2020-12-17 11:08:17 +0000

    more tuning

--- coursework/coursework.py
+++ coursework/coursework.py
@@ -6,6 +6,6 @@ tournament = Tournament(definition_file="1994_Formula_One.wmg")
 
 initial_ranking = Ranking(order=list(range(0, len(tournament.participants))), tournament=tournament)
 
-final_ranking = tournament.get_best_ranking(initial_ranking=initial_ranking, initial_temp=100, temp_length=50, cooling_ratio=0.995, num_non_improve=8000)
+final_ranking = tournament.get_best_ranking(initial_ranking=initial_ranking, initial_temp=152, temp_length=46, cooling_ratio=0.99, num_non_improve=18500)
 
 print(final_ranking.get_kemeny_score(), final_ranking.calc_kemeny_score(), final_ranking.get_participants())
